d44054a2f6af93279500a950844a450f854e128d
John Wise <jwise@physics.gatech.edu>
2021-02-10 10:35:13 -0500

    Compatible with latest ytree version

--- redshift_and_time.py
+++ redshift_and_time.py
@@ -5,23 +5,29 @@
 #
 ################################################################################
 
+import os
 import yt
 import ytree
 import numpy as np
 
 # Load datasets and arbor
-ts = yt.load('wise_data/DD00??/output_00??')
-a = ytree.load('wise_tree_0_0_0.dat')
+datadir = '/home/jwise/runs/SG64-2020/SG64-L2'
+ts = yt.load('%s/DD????/output_????' % (datadir))
+a = ytree.load('%s/rockstar_halos/trees/tree_0_0_0.dat' % (datadir))
 
 # Determine number of datasets.
 N = len(ts)
 
-# Loop through halos in the arbor to find one that has a progenitor line going
-# all the way to the first dataset. This will be the root halo whose progenitor
-# line will be used as reference for redshifts.
+# Loop through halos in the arbor to find the progenitor line with the longest history
+# This will be the root halo whose progenitor line will be used as reference for redshifts.
+maxN = -1
+ref_halo = None
 for root_halo in a:
-    if len(root_halo['prog']) == N:
-           break
+    if root_halo['prog', 'id'].size > maxN:
+        maxN = root_halo['prog', 'id'].size
+        ref_halo = root_halo
+print("Storing %d of %d redshifts that have halo data" % (maxN, N))
+N = maxN
 
 # There are slight discrepancies between redshifts stored in the datasets and
 # redshifts in the saved ytree arbor so save those as separate arrays.
@@ -31,14 +37,15 @@ cosmological_times = np.zeros(N)
 
 # Loop through each dataset and halo in the progenitor line to save redshifts
 # and cosmological time.
-# Reverse order ofs progenitor line to go in increasing time.
-for i, ds in enumerate(ts):
-    halo = root_halo['prog'][::-1][i]
-    ytree_redshifts[i] = halo['redshift']
+# Reverse order of progenitor line to go in increasing time.
+for i, ds in enumerate(ts[-N:]):
+    ytree_redshifts[i] = ref_halo['prog', 'redshift'][-i]
     ds_redshifts[i] = ds.current_redshift
     cosmological_times[i] = ds.current_time.to('yr')
 
 # Save all arrays to directory for later use.
+if not os.path.exists('stored_arrays'):
+    os.mkdir('stored_arrays')
 np.save('stored_arrays/ytree_redshifts', ytree_redshifts)
 np.save('stored_arrays/ds_redshifts', ds_redshifts)
 np.save('stored_arrays/cosmological_times', cosmological_times)
